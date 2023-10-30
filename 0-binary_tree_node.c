624b83bb676bf7302e193b7b5ca51909ed2a88b5
jsump <libbyn79@gmail.com>
2023-10-30 05:06:11 -0700

    Edit Header file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (node == NULL)
 		return (NULL);
 
-	node->value = value;
+	node->n = value;
 	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
